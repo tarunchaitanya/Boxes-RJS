d2246a5a4089aae2fc7471a40cd380a5c910cf4a
tarunchaitanya <tarunchaitanya.civil@gmail.com>
2021-10-16 04:50:19 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Box = (props) => {
+  //  Write your code here.
+  const { text, className } = props;
+  return (
+    <div className={`box ${className}`}>
+      <p className="box-title">{text}</p>
+    </div>
+  );
+};
+
+const element = (
+  //  Write your code here.
+  <div className="main-container">
+    <h1 className="heaading">Boxes</h1>
+    <div className="box-container">
+      <Box text="Small" className="small-box" />
+      <Box text="Medium" className="medium-box" />
+      <Box text="Large" className="large-box" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
